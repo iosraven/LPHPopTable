402b77842f59ca5e4e47f070b98bda6b91af66b7
mjq <535423469@qq.com>
2018-09-13 11:57:12 +0800

    去掉警告

--- example/LPHPopTable/LPHPopTable/ReadMe.h
+++ example/LPHPopTable/LPHPopTable/ReadMe.h
@@ -17,4 +17,38 @@
         NSLog(@"点击第 %ld 个",index);
     }];
  
+ 
+ 
+    3.  示例：定制视图样式大小与箭头坐标
+ 
+    #define Main_Screen_Height      [[UIScreen mainScreen] bounds].size.height
+    #define Main_Screen_Width       [[UIScreen mainScreen] bounds].size.width
+ 
+        NSArray *arr = @[
+                        @{@"Text":@"更改状态",@"Pass":@1},
+                        @{@"Text":@"作废",@"Pass":@1},
+                        @{@"Text":@"取消",@"Pass":@0}];
+ 
+        CGRect viewCGRect;
+        if((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad))
+        {
+            viewCGRect = CGRectMake(Main_Screen_Width - 300, Main_Screen_Height - 300, 300, 210);
+        }else
+        {
+            viewCGRect = CGRectMake(Main_Screen_Width - 210, Main_Screen_Height - 300, 200, 210);
+        }
+ 
+        CGPoint point = CGPointMake(viewCGRect.size.width * 3/4 + viewCGRect.origin.x, viewCGRect.origin.y +viewCGRect.size.height);
+ 
+ 
+        [LPHPopTable popWithArray:arr
+                            title:@"更多功能"
+                        viewCGRect:viewCGRect
+                            point:point
+                        showImage:YES
+                        popBlock:^(NSInteger index) {
+ 
+ 
+                        }];
+ 
  */
